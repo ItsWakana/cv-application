02618ea80f656c8f17f312aa2196520ed175d7d3
wakana <wakanawardrobe@gmail.com>
2023-03-29 15:34:17 +0100

    add rough component structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div className="App">
+    <div>
+      {/* add a component here which renders a form for adding personal information like name, age, address and includes a save button and an edit button. */}
+
+      {/* Personal Info component */}
+
+      {/* Work experience component */}
+
+      {/* Education component */}
+
+      
     </div>
+    
   );
 }
 
