15e5481633949e9011db1ddbfa10f0c2fa9f9ff0
wakana <wakanawardrobe@gmail.com>
2023-03-29 16:06:43 +0100

    call personal info module in app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import PersonalInfo from "./components/PersonalInfo";
+
 function App() {
   return (
     <div>
       {/* add a component here which renders a form for adding personal information like name, age, address and includes a save button and an edit button. */}
 
       {/* Personal Info component */}
-
+      <PersonalInfo />
       {/* Work experience component */}
 
       {/* Education component */}
