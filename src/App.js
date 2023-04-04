e930c23534506557e121f6a7b62ed37c23e6ef7c
wakana <wakanawardrobe@gmail.com>
2023-04-04 13:17:24 +0100

    complete main state with an object containing all of the form information

--- src/App.js
+++ src/App.js
@@ -2,26 +2,9 @@ import PersonalInfo from "./components/PersonalInfo";
 import WorkExperience from "./components/WorkExperience";
 import EducationExperience from "./components/EducationExperience";
 import style from './styles/style.css';
-
 import React, { Component } from 'react';
 
-// function App() {
-//   return (
-//     <div className="cv-form">
-//       {/* add a component here which renders a form for adding personal information like name, age, address and includes a save button and an edit button. */}
 
-//       {/* Personal Info component */}
-//       <PersonalInfo />
-//       {/* Work experience component */}
-//       <WorkExperience />
-//       {/* Education component */}
-//       <EducationExperience />
-
-      
-//     </div>
-    
-//   );
-// }
 class App extends Component {
   constructor(props) {
     super(props);
@@ -29,7 +12,8 @@ class App extends Component {
     this.state = {
       personalInfo: {},
       workExperience: {},
-      educationExperience: {}
+      educationExperience: {},
+      displayDetails: false,
     }
 
     this.setPersonalInfo = this.setPersonalInfo.bind(this);
